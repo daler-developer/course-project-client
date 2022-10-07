1acd1155e88d1c5956685176a8974435b834f304
Daler <daler.developer@mail.ru>
2022-10-07 15:37:53 +0500

    fixed

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://localhost:4000/api/:path*"
-            : "https://daler-course-project-server.herokuapp.com/api",
+            : "https://daler-course-project-server.herokuapp.com/api/:path*",
       },
       {
         source: "/uploads/:path*",
