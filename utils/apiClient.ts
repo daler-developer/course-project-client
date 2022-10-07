88ba9443df3a386abe7b4b5fd0c0fd7dcd39f4a3
Daler <daler.developer@mail.ru>
2022-10-07 15:29:51 +0500

    things for heroku deployment

--- utils/apiClient.ts
+++ utils/apiClient.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-const apiClient = axios.create({});
+console.log(process.env.NODE_ENV);
+
+const apiClient = axios.create({
+  ...(process.env.NODE_ENV === "production" && {
+    baseURL: "https://daler-course-project-server.herokuapp.com",
+  }),
+});
 
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
